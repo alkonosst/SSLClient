8cb81334f1582d41645170b70d821a4edda576ac
alkonosst <m.ramirez.bravo.94@gmail.com>
2023-05-03 10:01:14 -0400

    chore: Change library names due to conflict

--- src/SSLClientESP32.cpp
+++ src/SSLClientESP32.cpp
@@ -1,5 +1,5 @@
 /*
-  SSLClient.cpp - Base class that provides Client SSL to ESP32
+  SSLClientESP32.cpp - Base class that provides Client SSL to ESP32
   Additions (c) 2011 Adrian McEwen.  All right reserved.
   Additions Copyright (C) 2017 Evandro Luis Copercini.
   Additions Copyright (C) 2019 Vadim Govorovski.
@@ -17,7 +17,7 @@
   Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
-#include "SSLClient.h"
+#include "SSLClientESP32.h"
 #include "esp_crt_bundle.h"
 #include <errno.h>
 
@@ -26,7 +26,7 @@
 #undef read
 
 
-SSLClient::SSLClient()
+SSLClientESP32::SSLClientESP32()
 {
     _connected = false;
 
@@ -43,7 +43,7 @@ SSLClient::SSLClient()
     _use_ca_bundle = false;
 }
 
-SSLClient::SSLClient(Client* client)
+SSLClientESP32::SSLClientESP32(Client* client)
 {
     _connected = false;
 
@@ -59,13 +59,13 @@ SSLClient::SSLClient(Client* client)
     _use_ca_bundle = false;
 }
 
-SSLClient::~SSLClient()
+SSLClientESP32::~SSLClientESP32()
 {
     stop();
     delete sslclient;
 }
 
-void SSLClient::stop()
+void SSLClientESP32::stop()
 {
     if (sslclient->client >= 0) {
         //sslclient->client->stop();
@@ -75,40 +75,40 @@ void SSLClient::stop()
     SSLClientLib::stop_ssl_socket(sslclient, _CA_cert, _cert, _private_key);
 }
 
-void SSLClient::setClient(Client* client) {
+void SSLClientESP32::setClient(Client* client) {
     sslclient->client = client;
 }
 
-int SSLClient::connect(IPAddress ip, uint16_t port)
+int SSLClientESP32::connect(IPAddress ip, uint16_t port)
 {
     if (_pskIdent && _psKey)
         return connect(ip, port, _pskIdent, _psKey);
     return connect(ip, port, _CA_cert, _cert, _private_key);
 }
 
-int SSLClient::connect(IPAddress ip, uint16_t port, int32_t timeout){
+int SSLClientESP32::connect(IPAddress ip, uint16_t port, int32_t timeout){
     _timeout = timeout;
     return connect(ip, port);
 }
 
-int SSLClient::connect(const char *host, uint16_t port)
+int SSLClientESP32::connect(const char *host, uint16_t port)
 {
     if (_pskIdent && _psKey)
         return connect(host, port, _pskIdent, _psKey);
     return connect(host, port, _CA_cert, _cert, _private_key);
 }
 
-int SSLClient::connect(const char *host, uint16_t port, int32_t timeout){
+int SSLClientESP32::connect(const char *host, uint16_t port, int32_t timeout){
     _timeout = timeout;
     return connect(host, port);
 }
 
-int SSLClient::connect(IPAddress ip, uint16_t port, const char *CA_cert, const char *cert, const char *private_key)
+int SSLClientESP32::connect(IPAddress ip, uint16_t port, const char *CA_cert, const char *cert, const char *private_key)
 {
     return connect(ip.toString().c_str(), port, CA_cert, cert, private_key);
 }
 
-int SSLClient::connect(const char *host, uint16_t port, const char *CA_cert, const char *cert, const char *private_key)
+int SSLClientESP32::connect(const char *host, uint16_t port, const char *CA_cert, const char *cert, const char *private_key)
 {
     log_d("Connecting to %s:%d", host, port);
     if(_timeout > 0){
@@ -127,11 +127,11 @@ int SSLClient::connect(const char *host, uint16_t port, const char *CA_cert, con
     return 1;
 }
 
-int SSLClient::connect(IPAddress ip, uint16_t port, const char *pskIdent, const char *psKey) {
+int SSLClientESP32::connect(IPAddress ip, uint16_t port, const char *pskIdent, const char *psKey) {
     return connect(ip.toString().c_str(), port, pskIdent, psKey);
 }
 
-int SSLClient::connect(const char *host, uint16_t port, const char *pskIdent, const char *psKey) {
+int SSLClientESP32::connect(const char *host, uint16_t port, const char *pskIdent, const char *psKey) {
     log_v("start_ssl_client with PSK");
     if(_timeout > 0){
         sslclient->handshake_timeout = _timeout;
@@ -148,7 +148,7 @@ int SSLClient::connect(const char *host, uint16_t port, const char *pskIdent, co
     return 1;
 }
 
-int SSLClient::peek(){
+int SSLClientESP32::peek(){
     if(_peek >= 0){
         return _peek;
     }
@@ -156,12 +156,12 @@ int SSLClient::peek(){
     return _peek;
 }
 
-size_t SSLClient::write(uint8_t data)
+size_t SSLClientESP32::write(uint8_t data)
 {
     return write(&data, 1);
 }
 
-int SSLClient::read()
+int SSLClientESP32::read()
 {
     uint8_t data = -1;
     int res = read(&data, 1);
@@ -171,7 +171,7 @@ int SSLClient::read()
     return data;
 }
 
-size_t SSLClient::write(const uint8_t *buf, size_t size)
+size_t SSLClientESP32::write(const uint8_t *buf, size_t size)
 {
     if (!_connected) {
         return 0;
@@ -184,7 +184,7 @@ size_t SSLClient::write(const uint8_t *buf, size_t size)
     return res;
 }
 
-int SSLClient::read(uint8_t *buf, size_t size)
+int SSLClientESP32::read(uint8_t *buf, size_t size)
 {
     int peeked = 0;
     int avail = available();
@@ -214,7 +214,7 @@ int SSLClient::read(uint8_t *buf, size_t size)
     return res + peeked;
 }
 
-int SSLClient::available()
+int SSLClientESP32::available()
 {
     int peeked = (_peek >= 0);
     if (!_connected) {
@@ -228,7 +228,7 @@ int SSLClient::available()
     return res+peeked;
 }
 
-uint8_t SSLClient::connected()
+uint8_t SSLClientESP32::connected()
 {
     uint8_t dummy = 0;
     read(&dummy, 0);
@@ -236,7 +236,7 @@ uint8_t SSLClient::connected()
     return _connected;
 }
 
-void SSLClient::setInsecure()
+void SSLClientESP32::setInsecure()
 {
     _CA_cert = NULL;
     _cert = NULL;
@@ -246,12 +246,12 @@ void SSLClient::setInsecure()
     _use_insecure = true;   
 }
 
-void SSLClient::setCACert (const char *rootCA)
+void SSLClientESP32::setCACert (const char *rootCA)
 {
     _CA_cert = rootCA;
 }
 
-void SSLClient::setCACertBundle(const uint8_t * bundle)
+void SSLClientESP32::setCACertBundle(const uint8_t * bundle)
 {
     if (bundle != NULL)
     {
@@ -263,22 +263,22 @@ void SSLClient::setCACertBundle(const uint8_t * bundle)
     }
 }
 
-void SSLClient::setCertificate (const char *client_ca)
+void SSLClientESP32::setCertificate (const char *client_ca)
 {
     _cert = client_ca;
 }
 
-void SSLClient::setPrivateKey (const char *private_key)
+void SSLClientESP32::setPrivateKey (const char *private_key)
 {
     _private_key = private_key;
 }
 
-void SSLClient::setPreSharedKey(const char *pskIdent, const char *psKey) {
+void SSLClientESP32::setPreSharedKey(const char *pskIdent, const char *psKey) {
     _pskIdent = pskIdent;
     _psKey = psKey;
 }
 
-bool SSLClient::verify(const char* fp, const char* domain_name)
+bool SSLClientESP32::verify(const char* fp, const char* domain_name)
 {
     if (!sslclient)
         return false;
@@ -286,7 +286,7 @@ bool SSLClient::verify(const char* fp, const char* domain_name)
     return SSLClientLib::verify_ssl_fingerprint(sslclient, fp, domain_name);
 }
 
-char *SSLClient::_streamLoad(Stream& stream, size_t size) {
+char *SSLClientESP32::_streamLoad(Stream& stream, size_t size) {
   char *dest = (char*)malloc(size+1);
   if (!dest) {
     return nullptr;
@@ -300,7 +300,7 @@ char *SSLClient::_streamLoad(Stream& stream, size_t size) {
   return dest;
 }
 
-bool SSLClient::loadCACert(Stream& stream, size_t size) {
+bool SSLClientESP32::loadCACert(Stream& stream, size_t size) {
   if (_CA_cert != NULL) free(const_cast<char*>(_CA_cert));
   char *dest = _streamLoad(stream, size);
   bool ret = false;
@@ -311,7 +311,7 @@ bool SSLClient::loadCACert(Stream& stream, size_t size) {
   return ret;
 }
 
-bool SSLClient::loadCertificate(Stream& stream, size_t size) {
+bool SSLClientESP32::loadCertificate(Stream& stream, size_t size) {
   if (_cert != NULL) free(const_cast<char*>(_cert));
   char *dest = _streamLoad(stream, size);
   bool ret = false;
@@ -322,7 +322,7 @@ bool SSLClient::loadCertificate(Stream& stream, size_t size) {
   return ret;
 }
 
-bool SSLClient::loadPrivateKey(Stream& stream, size_t size) {
+bool SSLClientESP32::loadPrivateKey(Stream& stream, size_t size) {
   if (_private_key != NULL) free(const_cast<char*>(_private_key));
   char *dest = _streamLoad(stream, size);
   bool ret = false;
@@ -333,7 +333,7 @@ bool SSLClient::loadPrivateKey(Stream& stream, size_t size) {
   return ret;
 }
 
-int SSLClient::lastError(char *buf, const size_t size)
+int SSLClientESP32::lastError(char *buf, const size_t size)
 {
     if (!_lastError) {
         return 0;
@@ -342,12 +342,12 @@ int SSLClient::lastError(char *buf, const size_t size)
     return _lastError;
 }
 
-void SSLClient::setHandshakeTimeout(unsigned long handshake_timeout)
+void SSLClientESP32::setHandshakeTimeout(unsigned long handshake_timeout)
 {
     sslclient->handshake_timeout = handshake_timeout * 1000;
 }
 
-void SSLClient::setAlpnProtocols(const char **alpn_protos)
+void SSLClientESP32::setAlpnProtocols(const char **alpn_protos)
 {
     _alpn_protos = alpn_protos;
 }
